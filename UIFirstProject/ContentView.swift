493a9e007dcd7afd1641374b38a6185d412e7ed0
Çağdaş Ekşi <cagdas_eksioglu@hotmail.com>
2023-05-09 13:06:04 +0300

    Initial Commit

---
+++ UIFirstProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UIFirstProject
+//
+//  Created by ceksi on 9.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIFirstProject/UIFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIFirstProjectApp.swift
+//  UIFirstProject
+//
+//  Created by ceksi on 9.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct UIFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
